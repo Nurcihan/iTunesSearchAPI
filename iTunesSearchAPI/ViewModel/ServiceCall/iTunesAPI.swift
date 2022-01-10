288ee38050d1162dad3a8b2604d6403326618362
Nurcihan Karayakalı <nurcihankarayakali@hotmail.com>
2022-01-11 00:44:45 +0300

    iTunesAPI.swift updated

--- iTunesSearchAPI/ViewModel/ServiceCall/iTunesAPI.swift
+++ iTunesSearchAPI/ViewModel/ServiceCall/iTunesAPI.swift
@@ -13,25 +13,6 @@ class iTunesAPI {
     let baseUrl = "https://itunes.apple.com/search?"
     var items :[ContentModel] = []
     
-    
-//    func fetchMedia(searchValue: String,
-//                    completion: @escaping (ITunesDataModel?, Error?) -> Void) {
-//        let urlString = NetworkConfiguration.basePath +
-//        NetworkConfiguration.searchPath +
-//        searchValue + NetworkConfiguration.path
-//        let url = URL(string: urlString)!
-//        let task = URLSession.shared.sendRequest(url: url,
-//                                                 completionHandler: { (result: ITunesDataModel?, response, error) in
-//            if let error = error {
-//                completion(nil, error)
-//            }
-//            completion(result, nil)
-//        })
-//        task.resume()
-//
-//    }
-//
-    
     func getContentResponseWithSearch(searchLink: URL, completion: @escaping ([SegmentModel]? , Int) -> ()){
         
         let task = URLSession.shared.sendRequest(url: searchLink, completionHandler:{(result: ResponseModel?, response, error) in
@@ -44,29 +25,11 @@ class iTunesAPI {
                 self.items.append(contentsOf: model)
                 completion(self.groupAndCreateSectionList(contentList:self.items) , model.count)
             }
-            
+            completion(nil, 0)
         })
         task.resume()
-        
-        
-        
-//        AF.request(searchLink).responseData {(data) in
-//            switch data.result {
-//            case let .success(rawResponse):
-//                do {
-//                    let response = try JSONDecoder().decode(ResponseModel.self, from: rawResponse)
-//                    self.items.append(contentsOf: response.results)
-//                    completion(self.groupAndCreateSectionList(contentList:self.items) , response.results.count)
-//                } catch {
-//                    print("Error: \(error)")
-//                    completion(nil,0)
-//                }
-//            case let .failure(error):
-//                print(error)
-//                completion(nil,0)
-//            }
-//        }
     }
+    
     func getServiceURL(word: String, itemsPerPage:Int, offset: Int)->URL{
         let url = URL(string: baseUrl + "term=\(word)&limit=\(itemsPerPage)&offset=\(offset)")!
         return url
